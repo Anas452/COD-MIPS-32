05e8aa676c7372edb23afb5e46b56e6a5f9235a0
anas124 <anasm9877@gmail.com>
2019-04-02 23:51:58 +0500

    lab4

--- Lab04/lab4b.asm
+++ Lab04/lab4b.asm
@@ -1,4 +1,5 @@
 .data 
+    var: .word 5, 9 , 32
 
       Celcius: .asciiz "Enter Temperature In Celcius: "
       Faheriniet: .asciiz "The Temperature in Faherimiet: "
@@ -17,16 +18,22 @@ main:
     syscall
     move $t1,$v0
 
-    addi $t2,$zero,9
-    addi $t3,$zero,5
+    # addi $t2,$zero,9
+    # addi $t3,$zero,5
+    la $t0,var
+
+    lw $t2 ,0($t0)
+    lw $t3,4($t0)
 
     div $t2,$t3
     mflo $t4
+    addi $t4,$t4,2
 
-    mul $t5,$t1, $t4			
+    add $t5   
+    # mul $t5,$t1, $t4			
     # mflo	$t5					
 
-    mul $t6,$t5,32
+    # mul $t6,$t5,32
 
     li $v0,4
     la $a0,Faheriniet
