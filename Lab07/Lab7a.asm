42736c438a3170eea2082ed86773effdb038815e
anas124 <anasm9877@gmail.com>
2019-04-24 23:56:22 +0500

    lab7

--- Lab07/Lab7a.asm
+++ Lab07/Lab7a.asm
@@ -37,6 +37,33 @@ main:
     move $a0,$t2
     syscall
 
+POWER:
+    
+    addi $sp,$sp,-12
+    sw $s0,0($sp)
+    sw $s1,4($sp)
+    sw $s2,8($sp)
+    sw $s3,12($sp)
+
+
+    move $s0,$a0
+    move $s1,$a1
+
+    addi $s2,$0,0
+    addi $s3,$0,1
+
+loop:   slt $t0,$s2,$s1
+        beq $t0,$0,OUT
+        mult $s0,$s3
+        mflo $s3
+        addi $s2,$0,1
+        j loop
+
+OUT: move $v0,$s3
+
+    
+
+
 .end main
 
 
